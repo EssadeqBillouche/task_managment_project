b87e18781d72177505ca8fc96a809a343403777c
MrNobodyCode <essadeq.billouche@gmail.com>
2024-09-10 11:31:04 +0100

    last version -- need dubg

---
+++ header.h
@@ -0,0 +1,36 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+#include<stdlib.h>
+
+struct task                        // struct has all task info 
+{
+    int task_id; 
+    char task_title[50]; 
+    char task_description[500];
+    char task_statut[24];
+    // time_t task_deadline;
+};
+extern task array_task[10]; /// array of the baove struct
+
+struct user // struct has al the user info 
+{
+    int user_id;
+    char user_name[30];
+};
+
+struct management_user_task // struct for manage ment the ralation betweent user id and tasks id
+{
+    int Mang_user_id;
+    int mang_task_id;
+};
+extern struct management_user_task mang_array[10];
+extern struct user array_user[10]; // array of above struct 
+extern struct task array_task[10]; /// array of the baove struct
+
+void Add_task_fun(int *task_counter,int *Id_track );
+
+#endif
